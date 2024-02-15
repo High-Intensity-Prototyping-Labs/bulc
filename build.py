0b738cb4876818f5e9aa20705284779f78d9555e
amellalalex <github@alexamellal.com>
2024-02-15 20:46:00 +0100

    Added some heredocs to Target.

--- build.py
+++ build.py
@@ -14,15 +14,19 @@ class Target():
         self.raw_depends = [ dep.name for entry in self.__deps if entry.name == 'dep' for dep in entry.deps ]
 
     def sources(self):
+        """Matches `raw_sources` file patterns (using glob) in the filesystem when called"""
         return [ src for raw_src in self.raw_sources for src in glob.glob(raw_src, recursive=True) ]
 
     def headers(self):
+        """Matches `raw_headers` file patterns (using glob) in the filesystem when called"""
         return [ inc for raw_inc in self.raw_headers for inc in glob.glob(raw_inc, recursive=True) ]
 
     def private(self):
+        """Matches `raw_private` file patterns (using glob) in the filesystem when called"""
         return [ pri for raw_pri in self.raw_private for pri in glob.glob(raw_pri, recursive=True) ]
 
     def depends(self):
+        """Return value of `raw_depends`"""
         return self.raw_depends
 
     def includes(self):
