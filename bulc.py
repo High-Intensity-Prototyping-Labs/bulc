260e3f09636b0e47dd9228b49e0768030362169d
amellalalex <github@alexamellal.com>
2024-02-16 02:48:03 +0100

    Using FileSystemLoader for jinja vs PackageLoader

--- bulc.py
+++ bulc.py
@@ -1,6 +1,6 @@
 import bulgogi as bul 
 import glob
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
 from pathlib import Path
 
 class Target():
