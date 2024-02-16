ab60030f2824a3caae9657039da36f309625d232
amellalalex <github@alexamellal.com>
2024-02-16 15:40:31 +0100

    Updated gen.py example to new Target.expand()

--- gen.py
+++ gen.py
@@ -5,4 +5,4 @@ project = bulc.Project(from_file='project.yaml')
 env = bulc.Environment(loader=bulc.FileSystemLoader('templates'), autoescape=bulc.select_autoescape()) 
 template = env.get_template('Makefile.jinja')
 
-print(template.render({ "target": project.targets[0].expand() }))
+print(template.render(project.targets[0].expand()))
