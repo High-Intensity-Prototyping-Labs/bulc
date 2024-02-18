6b48348d069b4fc059d777c7987c29a38ba48e9f
amellalalex <github@alexamellal.com>
2024-02-18 18:21:06 +0100

    Now rendering entire project expansion w/ gen.py

--- gen.py
+++ gen.py
@@ -3,9 +3,12 @@ import bulc
 project = bulc.Project(from_file='project.yaml')
 
 env = bulc.Environment(loader=bulc.FileSystemLoader('templates'), autoescape=bulc.select_autoescape()) 
-Makefile_target = env.get_template('Makefile.target.jinja')
-# Makefile_project = env.get_template('Makefile.project.jinja')
+# Makefile_target = env.get_template('Makefile.target.jinja')
+Makefile_project = env.get_template('Makefile.project.jinja')
 
-print(Makefile_target.render({
-    "target": project.targets()[0].expand()
-}))
+# print(Makefile_target.render({
+#     "target": project.targets()[0].expand()
+# }))
+
+# print(project.expand())
+print(Makefile_project.render({ "project": project.expand() }))
