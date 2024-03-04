a47f6c66018808a2d0f95ed6d5b9eb29dd71ca77
amellalalex <github@alexamellal.com>
2024-03-04 21:37:43 +0100

    Modified gen.py to use new Makefile template.

--- gen.py
+++ gen.py
@@ -4,9 +4,10 @@ project = bulc.Project(from_file='project.yaml')
 
 env = bulc.Environment(loader=bulc.FileSystemLoader('templates'), autoescape=bulc.select_autoescape()) 
 # Makefile_target = env.get_template('Makefile.target.jinja')
-Makefile_project = env.get_template('Makefile.project.jinja')
+# Makefile_project = env.get_template('Makefile.project.jinja')
+Makefile = env.get_template('Makefile.jinja')
 
-print(Makefile_project.render({"project": project}))
+print(Makefile.render({"project": project}))
 # for target in project.expand().get('targets', []):
 #     print(target)
 #     print()
